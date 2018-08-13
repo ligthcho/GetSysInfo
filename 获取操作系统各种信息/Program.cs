0692f9771dec6e2e4182398ce158d1a86731aea2
lightcho <853358540@qq.com>
2018-08-13 11:29:23 +0800

    添加项目文件。

---
+++ 获取操作系统各种信息/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 获取操作系统各种信息
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
