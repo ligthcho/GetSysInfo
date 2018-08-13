93bdc4942d459928c4ef61183cb725d17f5c8b32
lightcho <853358540@qq.com>
2018-08-13 11:33:36 +0800

    练习

--- 获取操作系统各种信息/Program.cs
+++ 获取操作系统各种信息/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace 获取操作系统各种信息
 {
@@ -6,7 +7,17 @@ namespace 获取操作系统各种信息
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			//判断是否为操作系统
+			Console.WriteLine("判断是否为Windows Linux OSX");
+			Console.WriteLine($"Linux:{RuntimeInformation.IsOSPlatform(OSPlatform.Linux)}");
+			Console.WriteLine($"OSX:{RuntimeInformation.IsOSPlatform(OSPlatform.OSX)}");
+			Console.WriteLine($"Windows:{RuntimeInformation.IsOSPlatform(OSPlatform.Windows)}");
+			//获取操作系统架构、名称
+			Console.WriteLine($"系统架构：{RuntimeInformation.OSArchitecture}");
+			Console.WriteLine($"系统名称：{RuntimeInformation.OSDescription}");
+			Console.WriteLine($"进程架构：{RuntimeInformation.ProcessArchitecture}");
+			Console.WriteLine($"是否64位操作系统：{Environment.Is64BitOperatingSystem}");
+			Console.ReadKey();
         }
     }
 }
